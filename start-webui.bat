d1cad01a5e94613f02328baaca2fbcb91197ad51
Staroom <31646341+eastmoe@users.noreply.github.com>
2023-03-05 10:18:53 +0800

    Update start-webui.bat

--- start-webui.bat
+++ start-webui.bat
@@ -1,4 +1,13 @@
-python -m venv env
-env\Scripts\activate.bat
+if exist env (
+   echo "启动虚拟环境..."
+   env\Scripts\activate.bat
+) else (
+   echo "不存在env目录，创建虚拟环境..."
+   python -m venv env
+   echo "启动虚拟环境..."
+   env\Scripts\activate.bat
+)
+echo "安装依赖..."
 pip install -r requirement.txt
+echo "启动..."
 python run.py
